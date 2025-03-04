f1a825a68e116d92aa1a0fa22cbfbebf2bed01c6
Abdul Basit <ab9133711@gmail.com>
2025-03-04 21:49:17 +0500

    fix bug GA

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-// import Features from "./components/ProductSection";
 import ProductSection from "./components/ProductSection";
 import AboutSection from "./components/AboutSection";
 import ContactSection from "./components/ContactSection";
 import TrackPageViews from "./pages/TrackPageViews";
+import ReactGA from "react-ga4";
+
+// ✅ Initialize Google Analytics with your Measurement ID
+ReactGA.initialize("G-038GV4L9GW"); // Replace with your actual GA4 Measurement ID
 
 const App = () => {
   return (
--- src/pages/TrackPageViews.jsx
+++ src/pages/TrackPageViews.jsx
@@ -1,18 +1,15 @@
-import { useEffect } from 'react'
-import { useLocation } from 'react-router-dom';
-import ReactGA from 'react-ga4'
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import ReactGA from "react-ga4";
 
 const TrackPageViews = () => {
-    const location = useLocation();
-    useEffect(() => {
-        ReactGA.send({
-            hitType: "pageview",
-            page: location.pathname
-        })
-    }, [location]);
+  const location = useLocation();
 
+  useEffect(() => {
+    ReactGA.send({ hitType: "pageview", page: location.pathname });
+  }, [location]);
 
   return null;
-}
+};
 
-export default TrackPageViews
+export default TrackPageViews;
